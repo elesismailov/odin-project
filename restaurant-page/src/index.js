62e2a9cc735300f0b071a9cdf8a375b2a6eb5fd9
eles linux <eleswastaken@gmail.com>
2021-12-21 18:05:08 +0600

    Add Scroll Memory

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -13,27 +13,47 @@ const landing = LandingPage()
 const menu = Menu()
 const reserve = ReservePage()
 
+landing.scrl = {top: 0, left: 0}
+menu.scrl = {top: 0, left: 0}
+reserve.scrl = {top: 0, left: 0}
+
 content.appendChild(tabs)
 content.appendChild(landing)
 tabs.sticky(tabs.home)
 // content.appendChild(reserve)
 
 tabs.home.onclick = function(event) {
+	reserve.scrl.top = window.scrollY
+	menu.scrl.top = window.scrollY
+
 	reserve.remove()
 	menu.remove()
 	content.appendChild(landing)
 	tabs.sticky(tabs.home)
+	window.scrollTo(landing.scrl)
 }
 tabs.menu.onclick = function(event) {
+	landing.scrl.top = window.scrollY
+	reserve.scrl.top = window.scrollY
+
 	landing.remove()
 	reserve.remove()
 	content.appendChild(menu)
 	tabs.sticky(tabs.menu)
+	window.scrollTo(menu.scrl)
 }
 tabs.reserve.onclick = function(event) {
+	landing.scrl.top = window.scrollY
+	menu.scrl.top = window.scrollY
+
 	landing.remove()
 	menu.remove()
 	content.appendChild(reserve)
 	tabs.sticky(tabs.reserve)
+	window.scrollTo(reserve.scrl)
 }
-
+/*
+PROBLEM: 
+when between tabs skipping the need scrolled tab,
+the scroll value is overriden
+*/
