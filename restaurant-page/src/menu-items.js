b10ecc80e59707a96784fda601e0f7807447ce37
eles linux <eleswastaken@gmail.com>
2021-12-21 09:28:24 +0600

    Add Menu Items Data

---
+++ restaurant-page/src/menu-items.js
@@ -0,0 +1,30 @@
+
+
+const menuItems = [
+	{
+		title: "Keep it up",
+		imgURL: "./assets/images/1.jpg"
+	},
+	{
+		title: "Fly Ahead",
+		imgURL: "./assets/images/2.jpg"
+	},
+	{
+		title: "Brain Bone",
+		imgURL: "./assets/images/3.jpg"
+	},
+	{
+		title: "Alias Down",
+		imgURL: "./assets/images/4.jpg"
+	},
+	{
+		title: "Veritgo Beach",
+		imgURL: "./assets/images/5.jpg"
+	},
+	{
+		title: "Downright Leave",
+		imgURL: "./assets/images/6.jpg"
+	},
+];
+
+export default menuItems;
