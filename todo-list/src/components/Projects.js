3da4e1a5a6b3e569d43a244cda29bee895d19b69
eles linux <eleswastaken@gmail.com>
2021-12-23 15:48:42 +0600

    Add Project Rendering

--- todo-list/src/components/Projects.js
+++ todo-list/src/components/Projects.js
@@ -1,11 +1,27 @@
 
 
-export default function Projects() {
+export default function ProjectsComponent(projects) {
 
 	const wrapper = document.createElement("div");
+	const ul = document.createElement("ul")
+
+	const keys = Object.keys(projects);
 
 	wrapper.id = "all-projects";
 
+	keys.forEach( key => {
+		const li = document.createElement("li");
+		const project = document.createElement("div");
+		const title = document.createElement("h2");
+
+		project.className = "project";
+		title.innerHTML = projects[key].title;
+		console.log(projects[key])
+		project.appendChild(title);
+		li.appendChild(project)
+		ul.appendChild(li)
+	})
+	wrapper.appendChild(ul);
 	return wrapper;
 
 }
--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -2,7 +2,7 @@
 import Todo from "./todo-factory.js";
 import Project from "./project-factory.js";
 import Sidebar from "./components/Sidebar.js";
-import Projects from "./components/Projects.js";
+import ProjectsComponent from "./components/Projects.js";
 
 import "./style.css";
 
@@ -22,20 +22,20 @@ projects.personal.addTask(Todo("Forth task", undefined, 2, true, ""))
 projects.personal.addTask(Todo("Fifth task", undefined, 0, false, ""))
 
 // console.log(projects)
-console.table(projects.personal.tasks)
+// console.table(projects.personal.tasks)
 projects.personal.addTask(Todo("1"))
 // setTimeout(() => projects.personal.addTask(Todo("2")), 0);
 // setTimeout(() => projects.personal.addTask(Todo("3")), 500);
 // setTimeout(() => projects.personal.addTask(Todo("This wont be here")), 1000);
 // setTimeout(() => projects.personal.addTask(Todo("4")), 1500);
 // setTimeout(() => console.table(projects.personal.tasks), 1500);
-setTimeout(() => console.log(projects.personal.tasks[3]), 1500);
-setTimeout(() => (projects.personal.tasks[3].edit("Title update", 3)), 2000);
-setTimeout(() => console.log(projects.personal.tasks[3]), 3000);
+// setTimeout(() => console.log(projects.personal.tasks[3]), 1500);
+// setTimeout(() => (projects.personal.tasks[3].edit("Title update", 3)), 2000);
+// setTimeout(() => console.log(projects.personal.tasks[3]), 3000);
 
 
 const sidebar = Sidebar(),
-	  projectsComponent = Projects();
+	  projectsComponent = ProjectsComponent(projects);
 
 root.appendChild(sidebar)
 root.appendChild(projectsComponent)
