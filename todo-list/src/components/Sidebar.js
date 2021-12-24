65cbc3a68d7442ca6071f3e3cca4c0e0bfb52871
eles linux <eleswastaken@gmail.com>
2021-12-24 07:17:36 +0600

    Style tooltip

--- todo-list/src/components/Sidebar.js
+++ todo-list/src/components/Sidebar.js
@@ -7,21 +7,21 @@ export default function Sidebar() {
 
 	wrapper.id = "sidebar";
 
-	nav.appendChild(SidebarIcon("P"))
-	nav.appendChild(SidebarIcon("T"))
-	nav.appendChild(SidebarIcon("#"))
+	nav.appendChild(SidebarIcon("P", 'Projects'))
+	nav.appendChild(SidebarIcon("T", 'Tasks'))
+	nav.appendChild(SidebarIcon("#", 'Tags'))
 
 	wrapper.appendChild(nav)
 	return (wrapper);
 }
 
-function SidebarIcon(i) {
+function SidebarIcon(i, ii) {
 	const icon = document.createElement("button")
 	icon.className = "sidebar-icon";
 
 	icon.innerHTML = i;
 
-	icon.appendChild(IconTooltip("💡 " + i))
+	icon.appendChild(IconTooltip("💡 " + ii))
 
 	return (icon);
 }
