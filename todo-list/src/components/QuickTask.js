d2dabc2b7cb1ed991028597b2a59e02c4272c024
eles linux <eleswastaken@gmail.com>
2021-12-25 15:58:59 +0600

    Create Quick-task component

--- todo-list/src/components/QuickTask.js
+++ todo-list/src/components/QuickTask.js
@@ -3,7 +3,20 @@
 export default function QuickTask() {
 
     const wrapper = document.createElement("div");
-    wrapper.id = 'quick-task'
+    wrapper.id = 'quick-task';
 
+    wrapper.innerHTML = `
+        <form>
+			<span class="priority priority-new"></span>
+			<input type="checkbox" ${0 ? "checked" : ""}>
+            <input type="text" placeholder="New Task..."/>
+            <select>
+                <option>None</option>
+                <option>!</option>
+                <option>!!</option>
+                <option>!!!</option>
+            </select>
+        </form>
+    `;
     return wrapper
 }
