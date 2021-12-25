0178037255dea05af651bc1defa685e43ea9dea7
eles linux <eleswastaken@gmail.com>
2021-12-25 19:41:25 +0600

    Set Up Task info dropdown

--- todo-list/src/components/Task.js
+++ todo-list/src/components/Task.js
@@ -4,14 +4,17 @@ import state from "../state.js";
 export default function Task(t, rerender, pr) {
     const li = document.createElement("li");
     li.innerHTML = `
-		<div class='task'>
-			<span class="priority priority-${t.priority}"></span>
-			<input type="checkbox" ${t.isComplete ? "checked" : ""}>
-			<p class="task-title ${t.isComplete ? "line-through" : ""}">${t.title}</p>
-			${!pr ? `<p class="project-title">${t.project}</p>`: ''}
-			<button class="edit-task"></button>
-			<button class="delete-task" data-id="${t.id}"></button>
-		</div>
+        <details class="details-task">
+            <summary class='task'>
+                <span class="priority priority-${t.priority}"></span>
+                <input type="checkbox" ${t.isComplete ? "checked" : ""}>
+                <p class="task-title ${t.isComplete ? "line-through" : ""}">${t.title}</p>
+                ${!pr ? `<p class="project-title">${t.project}</p>`: ''}
+                <button class="edit-task"></button>
+                <button class="delete-task" data-id="${t.id}"></button>
+            </summary>
+            content of the thinggie
+        </details>
 	`;
     let keys = Object.keys(state.projects)
     let name = keys.filter(key => state.projects[key].title === t.project)
