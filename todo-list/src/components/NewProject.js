56d7ac92e18aafc96cbe73ffc5bcf5b962a6f3a1
eles linux <eleswastaken@gmail.com>
2021-12-27 17:04:27 +0600

    Create NewProject component

--- todo-list/src/components/NewProject.js
+++ todo-list/src/components/NewProject.js
@@ -5,5 +5,17 @@ export default function NewProject() {
     const wrapper = document.createElement("div");
     wrapper.id = 'new-project';
 
+    let html = `
+        <form>
+            <label>
+                <span class="plus">+</span>
+                <input name='title' type="text" placeholder="New Project..."/>
+            </label>
+            <button type="submit">Add</button>
+        </form>
+    `;
+    wrapper.innerHTML = html;
+
+    
     return wrapper;
 }
