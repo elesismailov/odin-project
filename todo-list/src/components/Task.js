dbf9f04e4b5ab7d161f8fdc0db8263cc175736c6
eles linux <eleswastaken@gmail.com>
2021-12-27 06:40:32 +0600

    Add edit form

--- todo-list/src/components/Task.js
+++ todo-list/src/components/Task.js
@@ -13,7 +13,22 @@ export default function Task(t, rerender, pr) {
                 <button class="edit-task  ${pr ? 'special' : ''}"></button>
                 <button class="delete-task" data-id="${t.id}"></button>
             </summary>
-            content of the thinggie
+            <div>
+                <form class="edit-form">
+                    <textarea name="description" id=""></textarea>
+                    <div>
+                        <label><input type="radio" name="priority" id="" value="0"/></label>
+                        <label><input type="radio" name="priority" id="" value="1"/></label>
+                        <label><input type="radio" name="priority" id="" value="2"/></label>
+                        <label><input type="radio" name="priority" id="" value="3"/></label>
+                    </div>
+                    <select name="project" id="">
+                        <option value="">Hello</option>
+                        <option value="">Hell</option>
+                        <option value="">Hel</option>
+                    </select>
+                </form>
+            </div>
         </details>
 	`;
     let keys = Object.keys(state.projects)
