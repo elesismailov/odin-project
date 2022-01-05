944949314bbe09e87018e46ca016a0393424f29a
eles linux <eleswastaken@gmail.com>
2022-01-05 16:01:03 +0600

    Solve test calculator

--- testing/custom/calculator.js
+++ testing/custom/calculator.js
@@ -1,24 +1,30 @@
 
 
 
+const calculator = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    divide: (a, b) => a / b,
+    multiply: (a, b) => a * b,
+}
 
 
 
 if (calculator.add(121, 212) !== 333) {
-    throw new Error()
+    throw new Error("STOPED!")
 } else { console.log( 'success ❎') }
 
 
 if (calculator.subtract(79, 2) !== 77) {
-    throw new Error()
+    throw new Error("STOPED!")
 } else { console.log( 'success ❎') }
 
 
-if (calculator.divide(144, 2) !== 77) {
-    throw new Error()
+if (calculator.divide(144, 2) !== 72) {
+    throw new Error("STOPED!")
 } else { console.log( 'success ❎') }
 
 
 if (calculator.multiply(32, 4) !== 128) {
-    throw new Error()
+    throw new Error("STOPED!")
 } else { console.log( 'success ❎') }
