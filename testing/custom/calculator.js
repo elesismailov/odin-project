9316bdcd10acd18f5f153182df12432be9f30bc7
eles linux <eleswastaken@gmail.com>
2022-01-05 15:57:20 +0600

    Add test calculatro

---
+++ testing/custom/calculator.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+if (calculator.add(121, 212) !== 333) {
+    throw new Error()
+} else { console.log( 'success ❎') }
+
+
+if (calculator.subtract(79, 2) !== 77) {
+    throw new Error()
+} else { console.log( 'success ❎') }
+
+
+if (calculator.divide(144, 2) !== 77) {
+    throw new Error()
+} else { console.log( 'success ❎') }
+
+
+if (calculator.multiply(32, 4) !== 128) {
+    throw new Error()
+} else { console.log( 'success ❎') }
