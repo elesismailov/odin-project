a1c65390cc96b49ad71142a2652fb47f6e952e80
eles linux <eleswastaken@gmail.com>
2022-01-06 08:53:53 +0600

    Solve for analyze array tests

--- testing/custom/analyzeArray.js
+++ testing/custom/analyzeArray.js
@@ -1,22 +1,51 @@
 
 
 
+function analyzeArray(arr) {
+
+    let average = arr.reduce( (init, n) => init + n ) / arr.length;
+    let min = arr.reduce( (init, n) => init > n ? n : init , arr[0]);
+    let max = arr.reduce( (init, n) => init < n ? n : init , arr[0]);
+
+    return {average, min, max, length: arr.length}
+}
+
 
 if ( analyzeArray([3, 4, 5, 6, 7]).average !== 5 ) {
     throw new Error("STOPED!")
 } else { console.log( 'success ❎') }
 
-
-if ( analyzeArray([3, 4, 5, 6, 7]).min !== 3 ) {
+if ( analyzeArray([3, 4, 6, 7]).average !== 5 ) {
     throw new Error("STOPED!")
 } else { console.log( 'success ❎') }
 
+
+
+if ( analyzeArray([5, 6, 3, 4, 5, 6, 7]).min !== 3 ) {
+    throw new Error("STOPED!")
+} else { console.log( 'success ❎') }
+
+if ( analyzeArray([5, 6, 7]).min !== 5 ) {
+    throw new Error("STOPED!")
+} else { console.log( 'success ❎') }
+
+
+
+if ( analyzeArray([3, 4, 5]).max !== 5 ) {
+    throw new Error("STOPED!")
+} else { console.log( 'success ❎') }
 
 if ( analyzeArray([3, 4, 5, 6, 7]).max !== 7 ) {
     throw new Error("STOPED!")
 } else { console.log( 'success ❎') }
 
 
+
+
 if ( analyzeArray([3, 4, 5, 6, 7]).length !== 5 ) {
+    throw new Error("STOPED!")
+} else { console.log( 'success ❎') }
+
+if ( analyzeArray([3, 4, 5, 6, 7, 7, 7, 7]).length !== 8 ) {
     throw new Error("STOPED!")
 } else { console.log( 'success ❎') }
