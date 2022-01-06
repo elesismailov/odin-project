328928a4d7e7dae33f63ead12f9ace2d1d8ecdef
eles linux <eleswastaken@gmail.com>
2022-01-06 08:44:03 +0600

    Add analyze array tests

---
+++ testing/custom/analyzeArray.js
@@ -0,0 +1,22 @@
+
+
+
+
+if ( analyzeArray([3, 4, 5, 6, 7]).average !== 5 ) {
+    throw new Error("STOPED!")
+} else { console.log( 'success ❎') }
+
+
+if ( analyzeArray([3, 4, 5, 6, 7]).min !== 3 ) {
+    throw new Error("STOPED!")
+} else { console.log( 'success ❎') }
+
+
+if ( analyzeArray([3, 4, 5, 6, 7]).max !== 7 ) {
+    throw new Error("STOPED!")
+} else { console.log( 'success ❎') }
+
+
+if ( analyzeArray([3, 4, 5, 6, 7]).length !== 5 ) {
+    throw new Error("STOPED!")
+} else { console.log( 'success ❎') }
