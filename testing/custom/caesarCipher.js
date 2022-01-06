3530e09d47396cc52d8bfdeb4161c0329da34db4
eles linux <eleswastaken@gmail.com>
2022-01-06 08:02:54 +0600

    Add caesarCipher tests

---
+++ testing/custom/caesarCipher.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+if ( caesarCipher('hello', 1) !== 'ifmmp') {
+    throw new Error("STOPED!")
+} else { console.log( 'success ❎') }
+
+
+if ( caesarCipher('bye', 3) !== 'ebh') {
+    throw new Error("STOPED!")
+} else { console.log( 'success ❎') }
+
+
+if ( caesarCipher('WorLd', 1) !== 'XpsMe') {
+    throw new Error("STOPED!")
+} else { console.log( 'success ❎') }
