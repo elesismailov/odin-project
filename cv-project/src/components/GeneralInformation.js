17071b4b629aa7a55ee2a517282a51e05eec9820
eles linux <eleswastaken@gmail.com>
2022-01-14 16:14:48 +0600

    Set up CV-Project

---
+++ cv-project/src/components/GeneralInformation.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+
+class GeneralInformation extends React.Component {
+
+    render() {
+        return (
+            <div id="general-information">
+                general info
+            </div>
+        );
+    }
+    
+}
