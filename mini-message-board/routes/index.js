3157529b038316c5ea653f785bdf81dfbbf844f3
eles linux <>
2022-01-25 13:25:58 +0600

    Set up message rendering

--- mini-message-board/routes/index.js
+++ mini-message-board/routes/index.js
@@ -16,7 +16,7 @@ const messages = [
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { messages, title: "Mini Message Board" });
 });
 
 module.exports = router;
