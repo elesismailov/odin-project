d7fc5ec331272ab2d523d38b0e249dfb317f07dc
eles linux <>
2022-01-25 09:34:57 +0600

    Connect and populate mongodb

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,8 +9,17 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+process.env.MONGODB_CONNECTION_URL
 
+var mongoose = require('mongoose');
+var mongoDB = process.env.MONGODB_CONNECTION_URL;
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('open', function() {console.log(db)})
+
+var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
