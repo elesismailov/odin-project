79d2adca07602ab396fcf658e8ec1d83bffaa46f
eles linux <>
2022-01-26 08:03:23 +0600

    Set up new genre form

--- express-locallibrary-tutorial/controllers/authorController.js
+++ express-locallibrary-tutorial/controllers/authorController.js
@@ -2,6 +2,7 @@ var Author = require('../models/author');
 var async = require('async');
 var Book = require('../models/book');
 
+
 // Display list of all Authors.
 exports.author_list = function(req, res, next) {
 
