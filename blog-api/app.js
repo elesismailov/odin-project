ba9a7a51df5b312cdfc074bf37a8e79a697eb1bd
eles linux <eleswastaken@gmail.com>
2022-02-08 08:24:25 +0600

    implement jwt token post creating

--- blog-api/app.js
+++ blog-api/app.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV === 'development') {
 
 
 const express = require("express");
+const jwt = require('jsonwebtoken');
 const path = require("path");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
@@ -50,24 +51,28 @@ app.get("/", (req, res) => {
 // GET All posts
 app.get('/all-posts', (req, res) => {
 	Post.find({}, 'title text date').exec((err, posts) => {
-		console.log(posts.map(p => p.url))
 		res.send(posts)
 	})
 })
 // CREATE new post
-app.post('/all-posts', (req, res) => {
-	if (req.body.title && req.body.text && req.body.date) {
-		new Post({
-			title: req.body.title,
-			text: req.body.text,
-			date: req.body.date
-		}).save((err) => {
-			if (err) {res.sendStatus(400)}
-			else {res.sendStatus(202)}
-		})
-	} else {
-		res.sendStatus(400)
-	}
+app.post('/all-posts', verifyToken, (req, res) => {
+	jwt.verify(req.token, process.env.JWT_KEY, (err, authData) => {
+		if (err) {res.sendStatus(403)}
+		else {
+			if (req.body.title && req.body.text && req.body.date) {
+				new Post({
+					title: req.body.title,
+					text: req.body.text,
+					date: req.body.date
+				}).save((err) => {
+					if (err) {res.sendStatus(400)}
+					else {res.sendStatus(201)}
+				})
+			} else {
+				res.sendStatus(400)
+			}
+		}
+	})
 })
 
 // Get ONE post
@@ -97,6 +102,32 @@ app.delete('/all-posts/:id', (req, res) => {
 		else { res.sendStatus(202) }
 	})
 })
+
+app.get('/log-in', (req, res) => {
+	// Here is supposed to be checking in database for the user
+	// MOCK user
+	const user = {
+		id: 0,
+		username: 'eles',
+		email: 'eleswastaken@gmail.com'
+	}
+
+	jwt.sign({user}, process.env.JWT_KEY, (err, token) => {
+		res.json({ token })
+	})
+})
+
+function verifyToken(req, res, next) {
+
+	const bearerHeader = req.headers['authorization'];
+	if (typeof bearerHeader !== 'undefined') {
+		const token = bearerHeader.split(' ')[1];
+		req.token = token
+		next()
+	} else {
+		res.sendStatus(403)
+	}
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Running on ${PORT}`));
