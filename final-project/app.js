8f89c4ad9686e23d684ec980341b3c9655ead10e
eles linux <eleswastaken@gmail.com>
2022-02-12 08:03:55 +0600

    connect mongodb

--- final-project/app.js
+++ final-project/app.js
@@ -5,6 +5,16 @@ if (process.env.NODE_ENV === 'development') {
 
 
 const express = require("express");
+
+const mongoose = require("mongoose");
+
+const mongodb = process.env.MONGODB_CONNECTION_URL;
+mongoose.connect(mongodb, {});
+const db = mongoose.connection;
+
+db.on("error", () => console.log("DB Connection error"));
+db.on('open', () => console.log('Ready for interactions'));
+
 const app = express();
 
 
