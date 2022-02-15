6d86591fe2643d85593865d24edafb0eab549709
eles linux <eleswastaken@gmail.com>
2022-02-15 14:54:19 +0600

    alter user model

--- final-project/models/user.js
+++ final-project/models/user.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema(
   {
     username: {type: String, required: true},
-    followings: [{type: Schema.Types.ObjectId, ref: 'User'}],
+    friends: [{type: Schema.Types.ObjectId, ref: 'User'}],
+    friendRequests: [{type: Schema.Types.ObjectId, ref: 'User'}],
   }
 );
 
