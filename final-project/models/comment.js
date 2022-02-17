985adfff31828353e48ae949d49bb0c9c1e0d36a
eles linux <eleswastaken@gmail.com>
2022-02-17 18:27:05 +0600

    alter comments model

--- final-project/models/comment.js
+++ final-project/models/comment.js
@@ -6,7 +6,7 @@ const CommentSchema = new Schema(
 	{
 		user: { type: Schema.Types.ObjectId, ref: 'User', required: true},
 		likes: [{type: Schema.Types.ObjectId, ref: 'User'}],
-
+		text: {type: String, required: true},
 	}
 )
 
