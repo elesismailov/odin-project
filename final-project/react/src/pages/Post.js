b941d6d05db4a98831ab8a43ce571f30f7cbf3a0
eles linux <eleswastaken@gmail.com>
2022-02-23 07:14:03 +0600

    implement react post/id post fetching

--- final-project/react/src/pages/Post.js
+++ final-project/react/src/pages/Post.js
@@ -1,17 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Post() {
 	
 	const { id } = useParams();
+	const [post, setPost] = useState(null);
+	const msg = useRef(null);
+
+	function showPost(post) {
+		msg.current.textContent = post.title;
+	}
+
 	useEffect(() => {
-		const response = fetch;
-		console.log(id)
+		const response = fetch( '/api/post/' + id, {
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': 'Bearer ' + localStorage.token,
+			},
+		});
+		response
+			.then(res => res.json())
+			.then(res => {console.log(res.post)})
 	});
-	
+
+	// the idea: instead of using refs, use state and dynamic rendering
+	// once the post is fetched, set the state, that will rerender the component
+	// add a conditional statement to the rendering
 	return (
 		<div>
-			<p>A post page</p>
+			<h1>A post page</h1>
+			<p ref={msg}>Loading...</p>
 		</div>
 	);
 }
