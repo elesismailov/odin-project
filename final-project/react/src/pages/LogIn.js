3d7cf095bce8febff11604f476240760c54db3f1
eles linux <eleswastaken@gmail.com>
2022-02-25 13:08:21 +0600

    implement log-in page redirect

--- final-project/react/src/pages/LogIn.js
+++ final-project/react/src/pages/LogIn.js
@@ -1,7 +1,41 @@
 
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
 function LogIn() {
+	const [ loggedIn, setLoggedIn ] = localStorage.token ? useState(true) : useState(false);
+
+	function handleSubmit(event) {
+		event.preventDefault()
+		const response = fetch('/api/log-in', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				email: event.target.email.value,
+				password: event.target.password.value
+			})
+		});
+		response
+			.then(res => res.json())
+			.then(res => {
+				saveToken(res.token);
+				setLoggedIn(true);
+			})
+			.catch(err => console.log('Email or password are wrong'))
+	}
+
+	function saveToken(token) {
+		localStorage.setItem('token', token)
+		// need to redirect to the /feed page
+	}
+	
+	if (loggedIn) {
+		return (
+			<Redirect to='/' />
+		);
+	}
 
 	return (
 		<div>
@@ -19,29 +53,6 @@ function LogIn() {
 			</form>
 		</div>
 	);
-}
-
-function handleSubmit(event) {
-	event.preventDefault()
-	const response = fetch('/api/log-in', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({
-			email: event.target.email.value,
-			password: event.target.password.value
-		})
-	});
-	response
-		.then(res => res.json())
-		.then(res => saveToken(res.token))
-		.catch(err => console.log('Email or password are wrong'))
-}
-
-function saveToken(token) {
-	localStorage.setItem('token', token)
-	// need to redirect to the /feed page
 }
 
 export default LogIn;
